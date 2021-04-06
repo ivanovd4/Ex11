74273c23a6031b0bc6f798576c0ce8ddfc1f47a0
Ivanov Daniil <daivanov_6@edu.hse.ru>
2021-04-06 23:57:05 +0300

    v2.2

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "train.h"
 
 int main() {
-  srand (time (NULL)); int len = 1000;
+  srand  (time (NULL)); int len = 1000;
   Train train(len);
   std::cout << calculate_len(train.first) << len << std::endl;
   return 0;
